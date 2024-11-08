5952a2b10454414975f2ce61cdf9d3737cc4998f
Kubab511 <barabasz.kuba@gmail.com>
2024-11-08 17:22:00 +0000

    Vercel compatibility

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/v1/*', (req, res) => {
+app.get('/api/*', (req, res) => {
   res.status(200).send('OK');
 });
 

36ea7e8095cea60316845bb0c924269dea3cf0d0
Kubab511 <barabasz.kuba@gmail.com>
2024-11-11 19:52:27 +0000

    test

--- api/v1.js
+++ api/v1.js
@@ -6,8 +6,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/api/v1/:request', (req, res) => {
-  console.log("Received request: " + req.params.request);
-  res.status(200).send('OK');
+  res.status(200).send("OK" + req.params.request);
 });
 
 module.exports = app;

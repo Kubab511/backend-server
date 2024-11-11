6a45a53dcbf630a6edf9c292400a793151365015
Kubab511 <111919220+Kubab511@users.noreply.github.com>
2024-11-11 13:02:58 +0000

    Fixed vercel

--- api/login.js
+++ api/login.js
@@ -24,7 +24,7 @@ const loadUsers = async () => {
 };
 
 // Define the /login route
-app.post('/api/login', async (req, res) => {
+app.get('/api/login', async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {

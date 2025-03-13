3435792238307de89437c2abbede7c0ebf37b4c8
Kuba Barabasz <barabasz.kuba@gmail.com>
2025-03-13 16:14:43 +0000

    update

--- api/server.js
+++ api/server.js
@@ -3,15 +3,15 @@ const crypto = require('crypto');
 const jsonfile = require('jsonfile');
 const path = require('path');
 const fs = require('fs');
-// const PORT = 5000;
-
 const app = express();
 
 app.use(express.json());
+
+
 const usersFilePath = path.join(__dirname, 'users.json');
 const keyPath = path.join(__dirname, 'key.pem')
-
 const key = fs.readFileSync(keyPath, 'utf8');
+
 
 const loadUsers = async () => {
   try {
@@ -23,13 +23,8 @@ const loadUsers = async () => {
   }
 };
 
-var date = new Date();
-date.setDate(date.getDate() + 7);
-date.setHours(23, 59, 59, 999);
-date = date.toISOString();
 
 app.post('/api/checkSerial', async (req, res) => {
-  console.log('Received JSON data:', req.body);
   const { deviceID, serial } = req.body;
 
   const users = await loadUsers();
@@ -39,11 +34,15 @@ app.post('/api/checkSerial', async (req, res) => {
     return res.status(401).send("");
   }
 
+  var date = new Date();
+  date.setDate(date.getDate() + 7);
+  date.setHours(23, 59, 59, 999);
+  date = date.toISOString();
+  
   let data = {
     deviceID: deviceID,
     expirationDate: date
   };
-
   data = JSON.stringify(data);
 
   const sign = crypto.createSign('sha256');
@@ -61,6 +60,3 @@ app.post('/api/checkSerial', async (req, res) => {
 });
 
 module.exports = app;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
